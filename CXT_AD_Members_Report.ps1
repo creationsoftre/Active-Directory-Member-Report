068483feb9020afafd27e797d65c6a3bd07141aa
creationsoftre <trevontejw@gmail.com>
2022-03-31 13:43:12 -0400

    Add files via upload

--- CXT_AD_Members_Report.ps1
+++ CXT_AD_Members_Report.ps1
@@ -1,6 +1,4 @@
-﻿#to get script to work. Complete a find and replace add domain & Add AD SamAccounts
-
-$cxt_SamAccounts = "Add AD SamAccounts"
+﻿$cxt_SamAccounts = "TP_GBD_Cxt_Dev_UserDev", "TP_GBD_Cxt_Dev_BusAdminDev", "TP_GBD_Cxt_Dev_SysAdminDev", "TP_GBD_Cxt_QA_User", "TP_GBD_Cxt_QA_SysAdmin", "TP_GBD_Cxt_QA_BusAdmin", "TP_GBD_Cxt_Prod_User", "TP_GBD_Cxt_Prod_SysAdmin", "TP_GBD_Cxt_Prod_BusAdmin"
 
 #create excel application
 $excel = New-Object -ComObject excel.application
@@ -26,19 +24,34 @@ foreach($cxt_samAccount in $cxt_SamAccounts)
     
     #Headers
     $workSheets.Cells.Item($row,$column) = "Name"
+    $workSheets.Cells.Item($row,$column).Font.Bold = $true
+    $workSheets.Cells.Item($row,$column).Font.Color = 8210719
+    $workSheets.Cells.Item($row,$column).Font.Size = 15
     $column++
     $workSheets.Cells.Item($row,$column) = "Account ID"
+    $workSheets.Cells.Item($row,$column).Font.Bold = $true
+    $workSheets.Cells.Item($row,$column).Font.Color = 8210719
+    $workSheets.Cells.Item($row,$column).Font.Size = 15
     $column++
     $workSheets.Cells.Item($row,$column) = "Creation of Account"
+    $workSheets.Cells.Item($row,$column).Font.Bold = $true
+    $workSheets.Cells.Item($row,$column).Font.Color = 8210719
+    $workSheets.Cells.Item($row,$column).Font.Size = 15
     $column++
     $workSheets.Cells.Item($row,$column) = "Last Log-On Date"
+    $workSheets.Cells.Item($row,$column).Font.Bold = $true
+    $workSheets.Cells.Item($row,$column).Font.Color = 8210719
+    $workSheets.Cells.Item($row,$column).Font.Size = 15
     $column++
     $workSheets.Cells.Item($row,$column) = "Enabled"
+    $workSheets.Cells.Item($row,$column).Font.Bold = $true
+    $workSheets.Cells.Item($row,$column).Font.Color = 8210719
+    $workSheets.Cells.Item($row,$column).Font.Size = 15
     
     #Resets column back to 1 for each worksheet
     $column = 1
     if($cxt_samAccount -like '*DEV*' -or $cxt_samAccount -like '*QA*'){
-        $info = Get-ADGroupMember -Identity $cxt_samAccount -Server 'add domain' | foreach{ get-aduser $_ -Properties *} | Select DisplayName, SamAccountName, whenCreated, LastLogonDate, Enabled, MemberOf
+        $info = Get-ADGroupMember -Identity $cxt_samAccount -Server 'devad.wellpoint.com' | foreach{ get-aduser $_ -Properties *} | Select DisplayName, SamAccountName, whenCreated, LastLogonDate, Enabled, MemberOf
 
         $row++
         for($i = 0 ; $i -lt $info.Length; $i++){
@@ -59,7 +72,7 @@ foreach($cxt_samAccount in $cxt_SamAccounts)
     }
 
     if($cxt_samAccount -like '*PROD*'){
-        $info = Get-ADGroupMember -Identity $cxt_samAccount -Server 'add domain'| foreach{ get-aduser $_ -Properties *} | Select DisplayName, SamAccountName, whenCreated, LastLogonDate, Enabled, MemberOf
+        $info = Get-ADGroupMember -Identity $cxt_samAccount -Server 'us.ad.wellpoint.com'| foreach{ get-aduser $_ -Properties *} | Select DisplayName, SamAccountName, whenCreated, LastLogonDate, Enabled, MemberOf
 
         $row++
         for($i = 0 ; $i -lt $info.Length; $i++){
@@ -80,7 +93,23 @@ foreach($cxt_samAccount in $cxt_SamAccounts)
     }
 
     $row = 1
+
+    #Auto fit everything so it looks better
+    $usedRange = $workSheets.UsedRange
+    $usedRange.EntireColumn.AutoFit() | Out-Null
 }
 
 #Delete Default Sheet
 $workbook.worksheets.item("Sheet1").Delete()
+
+#Save the file
+$workbook.SaveAs("\\va01pstodfs003.corp.agp.ads\apps\Local\EMT\COTS\McKesson\ClaimsXten\Active Directory\CXT_AD_Users_List.xlsx")
+
+#close workbook
+#$workbook.Close
+
+#Quit the application
+$excel.Quit()
+
+#Release COM Object
+[System.Runtime.InteropServices.Marshal]::ReleaseComObject([System.__ComObject]$excel) | Out-Null
